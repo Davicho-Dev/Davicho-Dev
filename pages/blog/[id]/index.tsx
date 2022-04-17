9de1a20960859f9c0cee7f44b345a1421bfa8b84
HarmonySkull <developer@harmonyskull.codes>
2022-04-17 04:15:33 -0500

    ADD: Blog pages;
    Signed-off-by: HarmonySkull <developer@harmonyskull.codes>

---
+++ pages/blog/[id]/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const index: FC = () => {
+	return <>Post</>
+}
+
+export default index
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const index: FC = () => {
+	return <>Blog</>
+}
+
+export default index
