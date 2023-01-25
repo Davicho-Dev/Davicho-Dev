bc0a570e0a551ff53e87c871a26285a0b99bf9f1
DavichoDev <13292121+davicho-dev@users.noreply.github.com>
2023-01-25 11:38:24 -0500

    UPDATE: Next config, README.md;
    Signed-off-by: DavichoDev <13292121+davicho-dev@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	// env: {
+	// 	baseURL: ''
+	// },
+	// images: {
+	// 	remotePatterns: [
+	// 		{
+	// 			protocol: 'https',
+	// 			hostname: 's3.amazonaws.com',
+	// 			pathname: '/media/**'
+	// 		}
+	// 	]
+	// },
+	experimental: {
+		appDir: true
+		// 	fontLoaders: [
+		// 		{
+		// 			loader: '@next/font/google',
+		// 			options: {
+		// 				fallback: ['DM Sans', 'Roboto'],
+		// 			},
+		// 		},
+		// 	],
+	}
 }
+// const withPWA = require('next-pwa')({
+// 	dest: 'public'
+// })
+//
+// module.exports = withPWA()
 
 module.exports = nextConfig
