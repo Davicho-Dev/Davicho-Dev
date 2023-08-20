ba5ad6d6c915c26a870afbaa34a00e8ce7d9d9f7
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-08-20 05:35:16 -0500

    fix(NextConfig): Remove sentry next config;
    Signed-off-by: DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs')
+// const { withSentryConfig } = require('@sentry/nextjs')
 
 /** @type {import('next').NextConfig} */
 
