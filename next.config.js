721624e625ced83d9d834ef59f852e5cc97b52f2
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-08-20 05:15:44 -0500

    feat(NextConfig): Remove sentry next config;
    Signed-off-by: DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -6,39 +6,48 @@ const { withSentryConfig } = require('@sentry/nextjs')
 
 /** @type {import('next').NextConfig} */
 
+// your existing module.exports or default export
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	// env: {
-	// 	baseURL: ''
-	// },
-	// images: {
-	// 	remotePatterns: [
-	// 		{
-	// 			protocol: 'https',
-	// 			hostname: 's3.amazonaws.com',
-	// 			pathname: '/media/**'
-	// 		}
-	// 	]
-	// },
-	experimental: {
-		appDir: true
-		// 	fontLoaders: [
-		// 		{
-		// 			loader: '@next/font/google',
-		// 			options: {
-		// 				fallback: ['DM Sans', 'Roboto'],
-		// 			},
-		// 		},
-		// 	],
-	}
+	// Optional build-time configuration options
+	sentry: {
+		// See the sections below for information on the following options:
+		//   'Configure Source Maps':
+		//     - disableServerWebpackPlugin
+		//     - disableClientWebpackPlugin
+		//     - hideSourceMaps
+		//     - widenClientFileUpload
+		//   'Configure Legacy Browser Support':
+		//     - transpileClientSDK
+		//   'Configure Serverside Auto-instrumentation':
+		//     - autoInstrumentServerFunctions
+		//     - excludeServerRoutes
+		//   'Configure Tunneling to avoid Ad-Blockers':
+		//     - tunnelRoute
+	},
 }
-// const withPWA = require('next-pwa')({
-// 	dest: 'public'
-// })
-//
-// module.exports = withPWA()
 
-module.exports = nextConfig
+// const sentryWebpackPluginOptions = {
+// Additional config options for the Sentry Webpack plugin. Keep in mind that
+// the following options are set automatically, and overriding them is not
+// recommended:
+//   release, url, configFile, stripPrefix, urlPrefix, include, ignore
 
-module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: true })
+// org: 'davichodev',
+// project: 'davichodev',
+
+// An auth token is required for uploading source maps.
+// You can get an auth token from https://sentry.io/settings/account/api/auth-tokens/
+// The token must have `project:releases` and `org:read` scopes for uploading source maps
+// authToken: process.env.SENTRY_AUTH_TOKEN,
+
+// silent: true, // Suppresses all logs
+
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options.
+// }
+
+// Make sure adding Sentry options is the last code to run before exporting
+// module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+
+// If you're using a next.config.mjs file:
+// export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
