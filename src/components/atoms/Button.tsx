41a05b1331f0c4343dfc563f0efadc34367d6b4b
Davicho-Dev <13292121+Davicho-Dev@users.noreply.github.com>
2024-12-31 10:04:32 -0500

    feat: Create button;
    Co-authored-by: HarmonySkull <109529692+HarmonySkull@users.noreply.github.com>
    Co-authored-by: DavichoDev <13292121+DavichoDev@users.noreply.github.com>
    Signed-off-by: Davicho-Dev <13292121+Davicho-Dev@users.noreply.github.com>

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,5 +1,8 @@
+import { Button2 } from '@atoms'
+
 export const Button = ({ label }: { label: string }): JSX.Element => (
 	<button title={label} aria-label={label}>
 		{label}
+		<Button2 label={label} />
 	</button>
 )
