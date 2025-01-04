c6071c1acc473bf7bd8a23a4e149f924b109d8cb
David Gavilanez <13292121+Davicho-Dev@users.noreply.github.com>
2025-01-04 16:50:43 +0000

    feat!: Framework version migrated to latest LTS version;
    Co-authored-by: HarmonySkull <109529692+HarmonySkull@users.noreply.github.com>
    Co-authored-by: Davicho-Dev <13292121+DavichoDev@users.noreply.github.com>
    Signed-off-by: David Gavilanez <13292121+Davicho-Dev@users.noreply.github.com>

--- src/components/atoms/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { Button2 } from '@atoms'
-
-export const Button = ({ label }: { label: string }): JSX.Element => (
-	<button title={label} aria-label={label}>
-		{label}
-		<Button2 label={label} />
-	</button>
-)
